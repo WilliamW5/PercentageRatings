106c47f2c70141ed5faf9ab03c9a29d52b0715c3
WilliamW5 <WilliamWiemann@outlook.com>
2021-07-25 00:24:42 -0400

    need to finish logic

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ from pandastable import Table, TableModel
 import dfTable
 
 def get_rating():
+    # Logic: get first split from textfield.
+    # Go to index location, then column name
+    # New x
+    # Go to new index location, then column name
+    values = text_field.get().split(",")
+    print(values)
     return
 
 root = Tk()
@@ -28,7 +34,7 @@ table.show()
 
 # 
 label = Label(root, text="Enter your ratings, seperated by a comma").pack()
-text_field = Entry(root, width=75, bg="#ffffff", fg="#000000", borderwidth=2).pack()
-button = Button(root, text="submit", command=get_rating()).pack()
+text_field = Entry(root, width=75, bg="#ffffff", fg="#000000", borderwidth=3).pack()
+button = Button(root, text="submit", command=get_rating).pack()
 
 root.mainloop()
